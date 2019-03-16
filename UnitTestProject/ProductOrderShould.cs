64c3dd6cf079a618d32f0ec933e859c163fa16d7
Chaitra <chaitradangat084@gmail.com>
2019-03-16 23:28:34 +0530

    created a test for checking the total price returned is proper

--- UnitTestProject/ProductOrderShould.cs
+++ UnitTestProject/ProductOrderShould.cs
@@ -1,7 +1,10 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+
+using NUnit.Framework;
+
+using UnitTestProject.Domain;
 
 namespace UnitTestProject
 {
@@ -12,10 +15,13 @@ namespace UnitTestProject
         public void ReturnTotalPriceOfOrder()
         {
             //arrange
+            var poFactory = new ProductOrderFactory(1,"Orange",100m,10);
 
             //act
+            var pOrder = poFactory.Build();
 
             //assert
+            Assert.That(100 * 10, Is.EqualTo(pOrder.TotalPrice));
         }
     }
 }
