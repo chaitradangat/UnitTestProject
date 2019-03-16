197b37904ab7ef6990c133f0142f7f10589d1884
Chaitra <chaitradangat084@gmail.com>
2019-03-16 22:47:47 +0530

    Console project to peek around in code..

---
+++ UnitTestProject.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnitTestProject.Domain;
+
+
+namespace UnitTestProject.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productOrder = new ProductOrderFactory(1, "Apples", 1000.05m, 1)
+                                    .WithSeasonDiscount()
+                                    .WithSpecialDiscount()
+                                    .Build();
+
+
+
+
+
+
+        }
+    }
+}
