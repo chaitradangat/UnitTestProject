97319ee1c4355edd767c3d7f264ca189aafb5046
Chaitra <chaitradangat084@gmail.com>
2019-03-16 22:51:58 +0530

    ProductOrder Factory for fluent construction of class

---
+++ UnitTestProject.Domain/ProductOrderFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestProject.Domain
+{
+    public class ProductOrderFactory
+    {
+        private int _ProductID;
+
+        private string _ProductName;
+
+        private decimal _Price;
+
+        private decimal _Quantity;
+
+        public ProductOrderFactory(int ProductID, string ProductName, decimal Price, decimal Quantity)
+        {
+            _ProductID = ProductID;
+            _ProductName = ProductName;
+            _Price = Price;
+            _Quantity = Quantity;
+        }
+
+        public ProductOrderFactory WithSeasonDiscount()
+        {
+            _Price = _Price / 20;
+
+            return this;
+        }
+
+        public ProductOrderFactory WithSpecialDiscount()
+        {
+            _Price = _Price / 10;
+
+            return this;
+        }
+
+        public ProductOrder Build()
+        {
+            return new ProductOrder { ProductID = _ProductID, ProductName = _ProductName, Price = _Price, Quantity = _Quantity };
+        }
+
+    }
+}
