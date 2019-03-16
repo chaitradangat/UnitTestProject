63cd203f514f246f13c5904a3aedf59b8a5696e6
Chaitra <chaitradangat084@gmail.com>
2019-03-16 23:17:20 +0530

    added NUnit attributes and Test Method

--- UnitTestProject/ProductOrderShould.cs
+++ UnitTestProject/ProductOrderShould.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTestProject
 {
-    class ProductOrderShould
+    [TestFixture]
+    public class ProductOrderShould
     {
+        [Test]
+        public void ReturnTotalPriceOfOrder()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
     }
 }
