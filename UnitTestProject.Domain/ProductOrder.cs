5390145a72aea0377e9da867d4ba8540f634eefa
Chaitra <chaitradangat084@gmail.com>
2019-03-16 16:21:59 +0530

    Added Product Order class

---
+++ UnitTestProject.Domain/ProductOrder.cs
@@ -0,0 +1,26 @@
+﻿
+namespace UnitTestProject.Domain
+{
+    public class ProductOrder
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        private decimal _TotalPrice;
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                _TotalPrice = Price * Quantity;
+                return _TotalPrice;
+            }
+        }
+
+    }
+}
