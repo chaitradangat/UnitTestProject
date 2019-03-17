a1180310f0b2d52d8d1a4b2890756d824596a089
Chaitra <chaitradangat084@gmail.com>
2019-03-17 12:08:09 +0530

    Changed order of parameters in Assert.That. Added more tests for better code coverage.

--- UnitTestProject/ProductOrderShould.cs
+++ UnitTestProject/ProductOrderShould.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 using UnitTestProject.Domain;
 
@@ -15,13 +11,67 @@ namespace UnitTestProject
         public void ReturnTotalPriceOfOrder()
         {
             //arrange
-            var poFactory = new ProductOrderFactory(1,"Orange",100m,10);
+            var poFactory = new ProductOrderFactory(1, "Orange", 100m, 10);
+            var sut = poFactory.Build();
 
             //act
-            var pOrder = poFactory.Build();
 
             //assert
-            Assert.That(100 * 10, Is.EqualTo(pOrder.TotalPrice));
+            Assert.That(sut.TotalPrice, Is.EqualTo(1000));
+        }
+
+        [Test]
+        public void SetTotalPriceOfOrder()
+        {
+            //arrange
+            var poFactory = new ProductOrderFactory(1, "Grapes", 200m, 10);
+            var sut = poFactory.Build();
+
+            //act
+            sut.TotalPrice = 100;
+
+            //assert
+            Assert.That(sut.TotalPrice, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void SetPriceOfOrder()
+        {
+            //arrange
+            var poFactory = new ProductOrderFactory(1, "Banana", 1000m, 100);
+            var sut = poFactory.Build();
+
+            //act
+            sut.TotalPrice = 500m;
+
+            //assert
+            Assert.That(sut.Price,Is.EqualTo(5));
+        }
+
+        [Test]
+        public void SetSeasonDiscount()
+        {
+            //arrange
+            var sut = new ProductOrderFactory(1, "JackFruit", 3000m, 100);
+
+            //act
+            sut = sut.WithSeasonDiscount();
+
+            //assert
+            Assert.That(sut.Price, Is.EqualTo(150));
+        }
+
+        [Test]
+        public void SetSpecialDiscount()
+        {
+            //arrange 
+            var sut = new ProductOrderFactory(2, "Mango", 500m, 1);
+
+            //act
+            sut = sut.WithSpecialDiscount();
+                            
+            //assert
+            Assert.That(sut.Price, Is.EqualTo(50));
         }
     }
 }
